4e6aa6fc14fbe9216ef90bd556be531ad1412c97
thesk-12 <150113005+thesk-12@users.noreply.github.com>
2026-09-18 14:08:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const welcomeButton = document.getElementById("welcomeButton");
+
+welcomeButton.addEventListener("click", function () {
+    alert("Welcome to my website! Thanks for visiting.");
+});
